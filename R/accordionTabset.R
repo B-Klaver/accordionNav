818bc417cb7ee6f84868b410eaa346a798e1a76d
Ding <joy.ding@bccdc.ca>
2024-09-04 16:07:46 -0700

    document() changes

--- R/accordionTabset.R
+++ R/accordionTabset.R
@@ -5,9 +5,14 @@
 #' @param menu_list The nested list with menu items and sub-items.
 #' @param class The custom CSS class for the sub-item menu items
 #' @importFrom purrr map
+#' @importFrom purrr pmap
 #' @importFrom shiny actionLink
+#' @importFrom shiny icon
 #' @importFrom bslib accordion
 #' @importFrom bslib accordion_panel
+#' @importFrom bsicons bs_icon
+#' @importFrom htmltools tags
+#' @importFrom htmltools HTML
 #' @return An accordion list for domain and sub-domain
 
 accordionTabset <- function(id, menu_list, class = NULL, icon_list = NULL) {
@@ -26,7 +31,7 @@ accordionTabset <- function(id, menu_list, class = NULL, icon_list = NULL) {
                 inputId = subitem_id,
                 label   = htmltools::tags$span(
                   htmltools::HTML("&nbsp;"),
-                  icon("angles-right"),
+                  shiny::icon("angles-right"),
                   htmltools::HTML("&nbsp;"),
                   subitem_id),
                 class   = class
@@ -35,13 +40,13 @@ accordionTabset <- function(id, menu_list, class = NULL, icon_list = NULL) {
         )
       )
   } else {
-    list(item_id = names(menu_list), icon = icon_list) |> 
+    list(item_id = names(menu_list), item_icon = icon_list) |> 
       purrr::pmap(
-        \(item_id, icon)
+        \(item_id, item_icon)
         bslib::accordion_panel(
           title = item_id,
           value = item_id,
-          icon  = bsicons::bs_icon(icon),
+          icon  = bsicons::bs_icon(item_icon),
           menu_list[[item_id]] |>
             purrr::map(
               \(subitem_id)
@@ -49,7 +54,7 @@ accordionTabset <- function(id, menu_list, class = NULL, icon_list = NULL) {
                 inputId = subitem_id,
                 label   = htmltools::tags$span(
                   htmltools::HTML("&nbsp;"),
-                  icon("angles-right"),
+                  shiny::icon("angles-right"),
                   htmltools::HTML("&nbsp;"),
                   subitem_id),
                 class   = class
